da2239279818c4b567197c7f847d193ee584b4fb
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2018-01-31 01:50:13 -0500

    Add throwIfNotAscii skeleton, attempt but fail to tokenize

--- src/haskell/lexer.hs
+++ src/haskell/lexer.hs
@@ -4,16 +4,20 @@ data Token = Token String String
 
 main :: IO ()
 main = do
-  contents <- getContents
+  -- Something is broken yere
+  contents <- throwIfNotAscii getContents
   let tokens = tokenize contents
-  map putTokens tokens
+  mapM_ putTokens tokens
 
 tokenize :: String -> [Token]
-tokenize x = splitWhen isSpace x
+tokenize x = map (const Token "id" "lexeme") (splitWhen isSpace x)
 
 putTokens :: Token -> IO ()
-putTokens (Token id lexeme) = putStrLn x
+putTokens (Token id lexeme) = putStrLn (id ++ " " ++ lexeme)
 
 -- This needs to be implemented
 splitWhen :: (a -> Bool) -> [a] -> [[a]]
-splitWhen pred (x:xs)
+splitWhen pred x = [x]
+
+-- This needs to be implemented
+throwIfNotAscii x = x
