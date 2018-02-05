606b7487eca3ecbd193f4a4e052cafd37fd7f048
Xia Liu <x397liu@uwaterloo.ca>
2018-02-04 22:30:14 -0500

    add more regular expressions

--- src/haskell/token-types.hs
+++ src/haskell/token-types.hs
@@ -7,33 +7,55 @@ tokenTypes = [
   Recognizer "javadoccommand" "",
 
   -- literal
-  Recognizer "intliteral" "",
-  Recognizer "boolliteral" "[]",
-  Recognizer "strliteral" "[]",
+  Recognizer "zeroliteral" "0",
+  Recognizer "pintliteral" "[1-9][0-9]*",
+  Recognizer "strliteral" "",
   Recognizer "nullliteral" "[]",
-  Recognizer "charliteral" "[]",
+  -- Recognizer "charliteral" "[~!@#$%^&*()_\"+,-./':;<=>?A-Za-z0-9[]_`{}|]",
   Recognizer "compoundnames" "[]",
   Recognizer "thisprimaryexpression" "[]",
   Recognizer "characterescapes" "[]",
 
   -- seperator
   Recognizer "comma" ",",
+  Recognizer "period" ".",
   Recognizer "semicolon" ";",
-  
+  Recognizer "lparenthese" "(",
+  Recognizer "rparenthese" ")",
+  Recognizer "lcurlybracket" "{",
+  Recognizer "rcurlybracket" "}",
+  Recognizer "lsquarebracket" "[",
+  Recognizer "rsquarebracket" "]",
+
   -- operator
-  Recognizer "equals" "=",
+  Recognizer "addition" "+",
+  Recognizer "subtract" "-",
+  Recognizer "multiplies" "*",
+  Recognizer "divides" "/",
+  Recognizer "greater" ">",
+  Recognizer "less" "<",
+  Recognizer "greater_equal" ">=",
+  Recognizer "less_equal" "<=",
+  Recognizer "equal" "==",
+  Recognizer "inequal" "!=",
+  Recognizer "bitwise_and" "&",
+  Recognizer "bitwise_or" "|",
+  Recognizer "negate" "!",
+  Recognizer "logical_and" "&&",
+  Recognizer "logical_or" "||",
 
   -- identifier
   Recognizer "identifier" "[a-zA-Z_][0-9a-zA-Z_]*",
 
 
   -- whitespace
-  Recognizer "space" "",
+  Recognizer "space" "[ ]*",
   Recognizer "newline" "",
     Recognizer "tab" ""
   ]
 
 keywords = []
 
+  Recognizer "boolliteral" "[]",
 
 invalidOperators = []
