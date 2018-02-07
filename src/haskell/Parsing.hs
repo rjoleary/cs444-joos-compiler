9dd78d6fe0af5fecfe956e57bd22726a1a2adaf7
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2018-02-07 01:02:31 -0500

    Add attribution in Lexer.hs

--- src/haskell/Parsing.hs
+++ src/haskell/Parsing.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+-- This code here is based on the paper Monadic Parsing in Haskell
+-- (http://www.cs.nott.ac.uk/~pszgmh/pearl.pdf) and the improvements
+-- made to it in
+-- http://vaibhavsagar.com/blog/2018/02/04/revisiting-monadic-parsing-haskell/
+
 module Parsing where
 
 import Control.Applicative
