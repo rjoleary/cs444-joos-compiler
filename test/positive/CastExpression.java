d7997f22d627c67e56ee6314205b81e17421d108
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2018-02-11 01:21:00 -0500

    Fix remaining conflicts in grammar

--- test/positive/CastExpression.java
+++ test/positive/CastExpression.java
@@ -1,6 +1,7 @@
 public class CastExpression {
-    public void f() {
+    public void f(CastExpression z) {
         int x = (int)-1;
         int y = (x)-1;
+        CastExpression w = (CastExpression)z;
     }
 }
