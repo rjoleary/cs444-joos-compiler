57b5370a568612ad71c8401a0da0f018d11fe072
Xia Liu <x397liu@uwaterloo.ca>
2018-02-11 00:55:25 -0500

    add more unit tests for tokentype

--- test/haskell/TokenTypesSpec.hs
+++ test/haskell/TokenTypesSpec.hs
@@ -9,7 +9,7 @@ runJoosParser x = maybeToIO $ runParser token x
 
 spec :: Spec
 spec = do
-  describe "joosToken" $ do
+  describe "Token" $ do
     it "parses recognized characters" $ do
       (tokens, s) <- runJoosParser "i"
       tokens `shouldBe` [Identifier "i"]
@@ -57,3 +57,152 @@ spec = do
         it "does not parse unicode escapes in strings" $ do
           (tokens, _) <- runJoosParser "\"\\u2297\""
           tokens `shouldBe` []
+
+      describe "int" $ do
+        it "parsers recognized integer zero" $ do
+          (tokens, _) <- runJoosParser "0"
+          tokens `shouldBe` [IntLiteral "0"]
+
+        it "parsers recognized nonzero intergers" $ do
+          (tokens, _) <- runJoosParser "12"
+          tokens `shouldBe` [IntLiteral "12"]
+
+        it "does not parse string starting with 0" $ do
+          (tokens, _) <- runJoosParser "012"
+          tokens `shouldBe` []
+    describe "operator" $ do
+      describe "assign" $ do
+        it "parses recognized assign" $ do
+          (tokens, _) <- runJoosParser "="
+          tokens `shouldBe` [Assign]
+
+      describe "add" $ do
+        it "parses recognized add" $ do
+          (tokens, _) <- runJoosParser "+"
+          tokens `shouldBe` [Add]
+
+      describe "subtract" $ do
+        it "parses recognized subtrect" $ do
+          (tokens, _) <- runJoosParser "-"
+          tokens `shouldBe` [Subtract]
+
+      describe "multiply" $ do
+        it "parses recognized multiply" $ do
+          (tokens, _) <- runJoosParser "*"
+          tokens `shouldBe` [Multiply]
+
+      describe "divide" $ do
+        it "parses recognized divide" $ do
+          (tokens, _) <- runJoosParser "/"
+          tokens `shouldBe` [Divide]
+
+      describe "modulus" $ do
+        it "parses recognized modulus" $ do
+          (tokens, _) <- runJoosParser "%"
+          tokens `shouldBe` [Modulus]
+
+      describe "greater" $ do
+        it "parses recognized greater" $ do
+          (tokens, _) <- runJoosParser ">"
+          tokens `shouldBe` [Greater]
+
+      describe "less" $ do
+        it "parses recognized less" $ do
+          (tokens, _) <- runJoosParser "<"
+          tokens `shouldBe` [Less]
+
+      describe "greaterequal" $ do
+        it "parses recognized greater equal" $ do
+          (tokens, _) <- runJoosParser ">="
+          tokens `shouldBe` [GreaterEqual]
+
+      describe "equal" $ do
+        it "parses recognized equal" $ do
+          (tokens, _) <- runJoosParser "=="
+          tokens `shouldBe` [Equal]
+
+      describe "lessequal" $ do
+        it "parses recognized lessequal" $ do
+          (tokens, _) <- runJoosParser "<="
+          tokens `shouldBe` [LessEqaul]
+
+      describe "inequal" $ do
+        it "parses recognized inequal" $ do
+          (tokens, _) <- runJoosParser "!="
+          tokens `shouldBe` [Inequal]
+
+      describe "bitwiseand" $ do
+        it "parses recognized bitwiseand" $ do
+          (tokens, _) <- runJoosParser "&"
+          tokens `shouldBe` [BitwiseAnd]
+
+      describe "bitwiseor" $ do
+        it "parses recognized bitwiseor" $ do
+          (tokens, _) <- runJoosParser "|"
+          tokens `shouldBe` [BitwiseOr]
+
+      describe "negate" $ do
+        it "parses recognized negate" $ do
+          (tokens, _) <- runJoosParser "!"
+          tokens `shouldBe` [Negate]
+
+      describe "logicaland" $ do
+        it "parses recognized logicaland" $ do
+          (tokens, _) <- runJoosParser "$$"
+          tokens `shouldBe` [LogicalAnd]
+
+      describe "logicalor" $ do
+        it "parsers recognized logicalor" $ do
+          (tokens, _) <- runJoosParser "||"
+          tokens `shouldBe` [LogicalOr]
+
+    describe "seperator"
+      describe "comma" $ do
+        it "parsers recognized comma" $ do
+          (tokens, _) <- runJoosParser ","
+          tokens `shouldBe` [Comma]
+      describe "period" $ do
+        it "parsers recognized period" $ do
+          (tokens, _) <- runJoosParser "."
+          tokens `shouldBe` [Period]
+
+      describe "leftBrace" $ do
+        it "parsers recognized leftBrace" $ do
+          (tokens, _) <- runJoosParser "{"
+          tokens `shouldBe` [LeftBrace]
+
+      describe "rightBrace" $ do
+        it "parsers recognized rightBrace" $ do
+          (tokens, _) <- runJoosParser "}"
+          tokens `shouldBe` [RightBrace]
+
+      describe "leftParen" $ do
+        it "parsers recognized leftParen" $ do
+          (tokens, _) <- runJoosParser "("
+          tokens `shouldBe` [LeftParen]
+
+      describe "rightParen" $ do
+        it "parsers recognized rightParen" $ do
+          (tokens, _) <- runJoosParser ")"
+          tokens `shouldBe` [RightParen]
+
+      describe "leftSquare" $ do
+        it "parsers recognized leftSquare" $ do
+          (tokens, _) <- runJoosParser "["
+          tokens `shouldBe` [LeftSquare]
+
+      describe "rightSquare" $ do
+        it "parsers recognized rightSquare" $ do
+          (tokens, _) <- runJoosParser "]"
+          tokens `shouldBe` [RightSquare]
+
+    describe "whitespace"
+      describe "space" $ do
+        it "parsers recognized space" $ do
+          (tokens, _) <- runJoosParser " "
+          tokens `shouldBe` [Space]
+
+      describe "comment" $ do
+        it "parsers recognized comment" $ do
+          (tokens, _) <- runJoosParser "/*//"
+          tokens `shouldBe` [Comment]
