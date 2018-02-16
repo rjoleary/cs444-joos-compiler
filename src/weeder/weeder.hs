5ce7b0a4e77b22415cc01a62809d6cfc7acbf694
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2018-02-16 08:29:59 -0500

    Create tagged tree

--- src/weeder/weeder.hs
+++ src/weeder/weeder.hs
@@ -4,7 +4,29 @@ import           Data.Maybe
 import           Data.Tree
 import           JoosCompiler.Exit
 
-type ParseTree = Tree String
+-- There are two types of weeder rules:
+-- 1. The simpler variety depend solely on the token names. These rules use
+--    UntaggedParseTree consisting of UntaggedToken.
+-- 2. After these rules are verified, extra attributes (start/end index) are
+--    attached to the tree and more weeder rules are checked. These rules use
+--    TaggedParseTree and TaggedToken. Tagged token containing enough
+--    information to rescue the original token string from the input file.
+
+type UntaggedParseTree = Tree UntaggedToken
+type UntaggedToken = String
+
+type TaggedParseTree = Tree TaggedToken
+data TaggedToken = TaggedToken { tokenName :: String
+                               , tokenString :: String
+                               , tokenStart :: Int
+                               , tokenEnd :: Int
+                               }
+instance Show TaggedToken where
+    show (TaggedToken name str start end) =
+            name
+            ++ (if name == str then "" else " \"" ++ str ++ "\"")
+            ++ (if start == end then "" else " (" ++ show start ++ "," ++ show end ++ ")")
+
 
 singleNode :: a -> Tree a
 singleNode x = Node x []
@@ -58,20 +80,20 @@ kNative :: String
 kNative = "native"
 
 -- Parse a single production rule into a tree with a single parent node.
-parseProduction :: String -> ParseTree
+parseProduction :: String -> UntaggedParseTree
 parseProduction line = Node rhs (map singleNode lhs)
   where
     rhs:lhs = words line
 
 -- Convert the bottom-up parse to a Haskell datatype.
-treeify :: String -> ParseTree
+treeify :: String -> UntaggedParseTree
 treeify x =
   case foldl treeify' [] (map parseProduction . lines $ x) of
     [t] -> t
     _   -> error "Could not create tree"
 
 -- Run this for each rule added to the tree.
-treeify' :: [ParseTree] -> ParseTree -> [ParseTree]
+treeify' :: [UntaggedParseTree] -> UntaggedParseTree -> [UntaggedParseTree]
 treeify' forest rule = rule' : forest'
   where
     lhsEq x y = lhs x == lhs y
@@ -81,29 +103,56 @@ treeify' forest rule = rule' : forest'
       Node (lhs rule) [fromMaybe x $ find (lhsEq x) forest | x <- rhs rule]
     forest' = deleteFirstsBy lhsEq forest (rhs rule)
 
-hasChild :: String -> ParseTree -> Bool
+-- Parse the tokens from joos_tokens.txt.
+type Token = (String, Int, Int)
+parseTokens :: String -> [Token]
+parseTokens = map ((\[x, y, z] -> (x, (read y), (read z))) . words) . lines
+
+-- Convert an untagged tree to a tagged tree with the tokens file.
+tagTree :: UntaggedParseTree -> [Token] -> TaggedParseTree
+tagTree utree tokens = if remainingTokens == []
+                       then taggedTree
+                       else error "Remaining tokens after tagging tree"
+    where (taggedTree, remainingTokens) = tagTree' utree tokens
+
+-- Recursively convert the tree and return any extra tokens.
+-- TODO: Tag inner nodes with (start, end) of their children.
+tagTree' :: UntaggedParseTree -> [Token] -> (TaggedParseTree, [Token])
+tagTree' (Node x []) [] = ((Node (TaggedToken x x 0 0) []), [])
+tagTree' (Node x []) a@((name, start, end):ts)
+    | x == name = ((Node (TaggedToken x x start end) []), ts)
+    | otherwise = ((Node (TaggedToken x x 0 0) []), a)
+tagTree' (Node x xs) ts = ((Node (TaggedToken x x 0 0) taggedChildren), remainingTokens)
+    where mapChildren :: [Tree UntaggedToken] -> [Token] -> ([Tree TaggedToken], [Token])
+          mapChildren [] ts = ([], ts)
+          mapChildren (n@(Node x xs):ns) ts = (taggedChild:ns', ts')
+              where (taggedChild, remainingTokens) = tagTree' n ts
+                    (ns', ts') = mapChildren ns remainingTokens
+          (taggedChildren, remainingTokens) = mapChildren xs ts
+
+hasChild :: String -> UntaggedParseTree -> Bool
 hasChild s tree =
   if (rootLabel tree) == s
     then True
     else any (hasChild s) $ subForest tree
 
-hasMethodBody :: ParseTree -> Bool
+hasMethodBody :: UntaggedParseTree -> Bool
 hasMethodBody tree
   | (lhs tree == kMethodBody) = hasChild kBlock tree
   | otherwise = any hasMethodBody (subForest tree)
 
-hasModifier :: String -> ParseTree -> Bool
+hasModifier :: String -> UntaggedParseTree -> Bool
 hasModifier modifier tree
   | rootLabel tree == modifier = True
   | otherwise = any (hasModifier modifier) $ subForest tree
 
-hasMethodsWithModifiers :: [String] -> ParseTree -> Bool
+hasMethodsWithModifiers :: [String] -> UntaggedParseTree -> Bool
 hasMethodsWithModifiers modifiers tree
   | (rootLabel tree) == kMethodDeclaration =
     any (\x -> hasModifier x tree) modifiers
   | otherwise = any (hasMethodsWithModifiers modifiers) $ subForest tree
 
-findChildren :: String -> ParseTree -> [ParseTree]
+findChildren :: String -> UntaggedParseTree -> [UntaggedParseTree]
 findChildren childName tree
   | (rootLabel tree) == childName = [tree]
   | otherwise = mconcat $ map (findChildren childName) $ subForest tree
@@ -111,14 +160,14 @@ findChildren childName tree
 -- Weeder rules
 -- Return true if check fails
 -- 1 A class cannot be both abstract and final
-notAbstractFinal :: ParseTree -> Bool
+notAbstractFinal :: UntaggedParseTree -> Bool
 notAbstractFinal tree =
   if (rootLabel tree) == kClassDeclaration
     then (hasModifier kAbstract tree) && (hasModifier kFinal tree)
     else any id $ map notAbstractFinal $ subForest tree
 
 -- 2 A method has a body if and only if it is neither abstract nor native.
-bodyIffNotAbstractOrNative :: ParseTree -> Bool
+bodyIffNotAbstractOrNative :: UntaggedParseTree -> Bool
 bodyIffNotAbstractOrNative tree
   | (rootLabel tree) == kMethodDeclaration =
     if (hasModifier kAbstract tree) || (hasModifier kNative tree)
@@ -127,7 +176,7 @@ bodyIffNotAbstractOrNative tree
   | otherwise = any bodyIffNotAbstractOrNative $ subForest tree
 
 -- 3 An abstract method cannot be static or final.
-abstractMethodNotStaticOrFinal :: ParseTree -> Bool
+abstractMethodNotStaticOrFinal :: UntaggedParseTree -> Bool
 abstractMethodNotStaticOrFinal tree
   | (rootLabel tree) == kMethodDeclaration =
     if (hasModifier kAbstract tree)
@@ -136,7 +185,7 @@ abstractMethodNotStaticOrFinal tree
   | otherwise = any abstractMethodNotStaticOrFinal $ subForest tree
 
 -- 4 A static method cannot be final.
-staticMethodNotFinal :: ParseTree -> Bool
+staticMethodNotFinal :: UntaggedParseTree -> Bool
 staticMethodNotFinal tree
   | (rootLabel tree) == kMethodDeclaration =
     if (hasModifier kStatic tree)
@@ -145,7 +194,7 @@ staticMethodNotFinal tree
   | otherwise = any staticMethodNotFinal $ subForest tree
 
 -- 5 A native method must be static.
-nativeMethodStatic :: ParseTree -> Bool
+nativeMethodStatic :: UntaggedParseTree -> Bool
 nativeMethodStatic tree
   | (rootLabel tree) == kMethodDeclaration =
     if (hasModifier kNative tree)
@@ -155,11 +204,11 @@ nativeMethodStatic tree
 
 -- TODO
 -- 8 A class/interface must be declared in a .java file with the same base name as the class/interface.
-classnameSameAsFilename :: ParseTree -> Bool
+classnameSameAsFilename :: UntaggedParseTree -> Bool
 classnameSameAsFilename tree = True
 
 -- 10 An interface method cannot be static, final, or native.
-interfaceMethodNotStaticFinalOrNative :: ParseTree -> Bool
+interfaceMethodNotStaticFinalOrNative :: UntaggedParseTree -> Bool
 interfaceMethodNotStaticFinalOrNative tree
   | (rootLabel tree) == kInterfaceDeclaration =
     hasMethodsWithModifiers [kStatic, kFinal, kNative] tree
@@ -167,21 +216,21 @@ interfaceMethodNotStaticFinalOrNative tree
 
 -- TODO
 -- 12 Every class must contain at least one explicit constructor.
-classAtLeastOneConstructor :: ParseTree -> Bool
+classAtLeastOneConstructor :: UntaggedParseTree -> Bool
 classAtLeastOneConstructor tree = True
 
 -- 13 No field can be final.
-noFinalField :: ParseTree -> Bool
+noFinalField :: UntaggedParseTree -> Bool
 noFinalField tree
   | (rootLabel tree) == kFieldDeclaration = hasModifier kFinal tree
   | otherwise = any noFinalField $ subForest tree
 
 -- TODO
-integerWithinRange :: ParseTree -> Bool
+integerWithinRange :: UntaggedParseTree -> Bool
 integerWithinRange tree = True
 
-weederRules :: [ParseTree -> Bool]
-weederRules =
+untaggedRules :: [UntaggedParseTree -> Bool]
+untaggedRules =
   [ notAbstractFinal
   , bodyIffNotAbstractOrNative
   , abstractMethodNotStaticOrFinal
@@ -191,12 +240,26 @@ weederRules =
   , noFinalField
   ]
 
-weed :: ParseTree -> Bool
-weed tree = any id (pam weederRules tree)
+taggedRules :: [TaggedParseTree -> Bool]
+taggedRules =
+  [
+  -- TODO
+  ]
+
+untaggedWeed:: UntaggedParseTree -> Bool
+untaggedWeed tree = any id (pam untaggedRules tree)
+
+taggedWeed :: TaggedParseTree -> Bool
+taggedWeed tree = any id (pam taggedRules tree)
 
 main :: IO ()
 main = do
   contents <- readFile "test/joos_tree.txt"
   let tree = treeify contents
-  when (weed tree) $ exitError "Bad weed"
+  when (untaggedWeed tree) $ exitError "Bad weed"
+
+  tokens <- readFile "test/joos_tokens.txt"
+  let taggedTree = tagTree tree (parseTokens tokens)
+  when (taggedWeed taggedTree) $ exitError "Bad weed"
+  putStrLn $ drawTree (fmap show taggedTree)
   return ()
