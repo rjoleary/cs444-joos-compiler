cbcd43ece44a5208811b85c6f1cbc8dd4df7380f
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2018-02-22 00:58:50 -0500

    Add some AST types

--- src/ast/Ast.hs
+++ src/ast/Ast.hs
@@ -10,18 +10,6 @@ data CompilationUnit = CompilationUnit
         }
         | EmptyFile
 
-data ClassDeclaration = ClassDeclaration
-        { isInterface    :: Bool
-        , classModifiers :: [Modifier]
-        , className      :: String
-        , super          :: Name
-        , interfaces     :: [Name]
-        , fields         :: [Field]
-        , methods        :: [Method]
-        , constructor    :: Maybe Method
-        }
-
-
 data Field = Field
         { fieldType      :: Type
         , fieldModifiers :: [Modifier]
@@ -29,7 +17,59 @@ data Field = Field
         , fieldValue     :: Expression
         }
 
--- TODO
+data Scope = Scope
+        { parentScope :: Maybe Scope
+        , scopeVariables :: [Field]
+        , statements :: [Statement]
+        }
+
+data ClassDeclaration = ClassDeclaration
+        { isInterface    :: Bool
+        , classModifiers :: [Modifier]
+        , className      :: String
+        , super          :: Name
+        , interfaces     :: [Name]
+        , classScope     :: Scope
+        , methods        :: [Method]
+        , constructor    :: Maybe Method
+        }
+
 data Method = Method
+        { methodType :: Type
+        , methodModifiers :: [Modifier]
+        , methodName :: Name
+        -- list of scopes where scope n is a parent of scope n + 1
+        -- (first methodScopes) is the scope of this method's class
+        , methodScopes :: [Scope]
+        }
+
+data Expression = MethodInvocation
+        { functionName :: Name
+        , arguments :: [Expression]
+        }
+        | BinOpApplication
+        { operator :: Operator
+        , leftOperand :: Expression
+        , rightOperand :: Expression
+        }
+        | UnaryOpApplication
+        { operator :: Operator
+        , operand :: Expression
+        }
+
+
+-- TODO: add constructor fields for statements
+data Statement = AssignStatement
+        { assignedVar :: Maybe Field
+        , assignedValue :: Expression
+        }
+        | BreakStatement
+        | ReturnStatement
+        | ControlFlowStatement
+
+data ControlFlowStatement = ForStatement
+        | WhileStatement
+        | IfStatement
+
+-- TODO
 data Type = Type
-data Expression = Expression
