2211857a8e160ccee6be474c675c430d0b25e7a2
Ahmed Al-Sudani <ahmed.al-sudani@uwaterloo.ca>
2018-03-07 22:48:39 -0500

    Restructure ExpressionTyping

--- src/lib/haskell/JoosCompiler/Ast/SecondaryProcessing/ExpressionTyping.hs
+++ src/lib/haskell/JoosCompiler/Ast/SecondaryProcessing/ExpressionTyping.hs
@@ -2,49 +2,63 @@ module JoosCompiler.Ast.SecondaryProcessing.ExpressionTyping
   ( typeAstExpressions
   ) where
 
+import           Data.Maybe
 import           Data.Tree
 import           JoosCompiler.Ast.NodeTypes
 import           JoosCompiler.Ast.NodeFunctions
 import           JoosCompiler.Ast.Transformers.Types
 import           JoosCompiler.Ast.Utils
 
-typeAstExpressions :: Maybe Scope -> AstNode -> AstNode
-typeAstExpressions _ (Node (AstBlock block) _children) =
+typeAstExpressions :: AstNode -> AstNode
+typeAstExpressions n = typeAstExpressionsInner Nothing Nothing n
+
+typeAstExpressionsInner :: Maybe CompilationUnit -> Maybe Scope -> AstNode -> AstNode
+typeAstExpressionsInner _ _ (Node (AstCompilationUnit unit) _children) =
+  Node (AstCompilationUnit unit) typedChildren
+  where
+    typedChildren = map (typeAstExpressionsInner (Just unit) $ Nothing) _children
+
+typeAstExpressionsInner cu _ (Node (AstBlock block) _children) =
   Node (AstBlock block) typedChildren
   where
     scope = blockScope block
-    typedChildren = map (typeAstExpressions $ Just scope) _children
+    typedChildren = map (typeAstExpressionsInner cu $ Just scope) _children
 
-typeAstExpressions scope (Node (AstExpression (Expression _ (MethodInvocation parentObjectName methodName arguments))) _children)
+typeAstExpressionsInner cu scope (Node (AstExpression e) _children) =
+  Node (AstExpression $ typedExpression) newChildren
+  where
+    newChildren = map (typeAstExpressionsInner cu scope) _children
+    typedExpression = typeExpression (fromJust cu) scope e
+
+
+typeExpression :: CompilationUnit -> Maybe Scope -> Expression -> Expression
+typeExpression cu scope (Expression _ (MethodInvocation parentObjectName methodName arguments))
   | (map localType $ methodParameters method) == map calculateType arguments = typedExpression
   | otherwise = error "Method signature does not match argument list types"
   where
     method = resolveMethod ["TODO"]
-    typedExpression = Node (AstExpression $ Expression _type (MethodInvocation parentObjectName methodName arguments)) newChildren
-    newChildren = map (typeAstExpressions scope) _children
+    typedExpression = Expression _type (MethodInvocation parentObjectName methodName arguments)
     _type = methodReturn method
 
-typeAstExpressions scope (Node (AstExpression (Expression _ l@(Literal _type v))) _children) =
-  Node (AstExpression $ Expression _type l) newChildren
-  where
-    newChildren = map (typeAstExpressions scope) _children
-
+typeExpression _ _ (Expression _ l@(Literal _type v)) = Expression _type l
 
 ------------------------BinaryOperation-------------------------------------------------------
-typeAstExpressions scope (Node (AstExpression (Expression _ (BinaryOperation operator e1 e2))) _children)
-  | and [(expressionType typedE1 ==  expressionType typedE2), (innerType (expressionType typedE1) == Int), not((isArray (expressionType typedE1))), (operator `elem` [Multiply, Divide, Modulus])]= typedExpression
-  | and [(expressionType typedE1 ==  expressionType typedE2), (innerType (expressionType typedE1) == Short), not((isArray (expressionType typedE1)))]= typedExpression
+typeExpression cu scope (Expression _ (BinaryOperation operator e1 e2))
+  | and [(expressionType typedE1 ==  expressionType typedE2),
+         (innerType (expressionType typedE1) == Int),
+         not((isArray (expressionType typedE1))),
+         (operator `elem` [Multiply, Divide, Modulus])] =
+      typedExpression
+  | and [(expressionType typedE1 ==  expressionType typedE2),
+         (innerType (expressionType typedE1) == Short),
+         not((isArray (expressionType typedE1)))] =
+      typedExpression
   | otherwise = error "Mutilply parameters are not the same and is invalid"
   where
-    typedExpression = Node (AstExpression $ Expression _type (BinaryOperation Multiply typedE1 typedE2)) newChildren
-    newChildren = map (typeAstExpressions scope) _children
+    typedExpression = Expression _type (BinaryOperation Multiply typedE1 typedE2)
     _type = expressionType e1
-    typedE1 = e1 -- typeAstExpressions scope e1
-    typedE2= e2 -- typeAstExpressions scope e2
-
-typeAstExpressions scope (Node n _children) = (Node n newChildren)
-  where
-    newChildren = map (typeAstExpressions scope) _children
+    typedE1 = typeExpression cu scope e1
+    typedE2= typeExpression cu scope e2
 
 -- TODO
 calculateType :: Expression -> Type
