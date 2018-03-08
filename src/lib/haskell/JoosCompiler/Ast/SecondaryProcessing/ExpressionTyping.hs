83089cafe406f7ab8fd4ebebb89ba4f2fdeb9200
Ahmed Al-Sudani <ahmed.al-sudani@uwaterloo.ca>
2018-03-07 22:08:00 -0500

    Rename addTypes -> typeAstExpressions

--- src/lib/haskell/JoosCompiler/Ast/SecondaryProcessing/ExpressionTyping.hs
+++ src/lib/haskell/JoosCompiler/Ast/SecondaryProcessing/ExpressionTyping.hs
@@ -8,49 +8,43 @@ import           JoosCompiler.Ast.NodeFunctions
 import           JoosCompiler.Ast.Transformers.Types
 import           JoosCompiler.Ast.Utils
 
-typeAstExpressions :: AstNode -> AstNode
-typeAstExpressions _t = addTypes Nothing _t
-
-addTypes :: Maybe Scope -> AstNode -> AstNode
-addTypes _ (Node (AstBlock block) _children) =
+typeAstExpressions :: Maybe Scope -> AstNode -> AstNode
+typeAstExpressions _ (Node (AstBlock block) _children) =
   Node (AstBlock block) typedChildren
   where
     scope = blockScope block
-    typedChildren = map (addTypes $ Just scope) _children
+    typedChildren = map (typeAstExpressions $ Just scope) _children
 
--- FIXME: the method might be called on an expression
--- FIXME: addTypes scope (Node (AstExpression (Expression _ (MethodInvocation expression name arguments))) _children)
-  -- FIXME: | (map localType $ methodParameters method) == map calculateType arguments = typedExpression
-  -- FIXME: | otherwise = error "Method signature does not match argument list types"
-  -- FIXME: where
-    -- FIXME: method = resolveMethod name
-    -- FIXME: typedExpression = Node (AstExpression $ Expression _type (MethodInvocation expression name arguments)) newChildren
-    -- FIXME: newChildren = map (addTypes scope) _children
-    -- FIXME: _type = methodReturn method
+typeAstExpressions scope (Node (AstExpression (Expression _ (MethodInvocation parentObjectName methodName arguments))) _children)
+  | (map localType $ methodParameters method) == map calculateType arguments = typedExpression
+  | otherwise = error "Method signature does not match argument list types"
+  where
+    method = resolveMethod ["TODO"]
+    typedExpression = Node (AstExpression $ Expression _type (MethodInvocation parentObjectName methodName arguments)) newChildren
+    newChildren = map (typeAstExpressions scope) _children
+    _type = methodReturn method
 
-addTypes scope (Node (AstExpression (Expression _ l@(Literal _type v))) _children) =
+typeAstExpressions scope (Node (AstExpression (Expression _ l@(Literal _type v))) _children) =
   Node (AstExpression $ Expression _type l) newChildren
   where
-    newChildren = map (addTypes scope) _children
+    newChildren = map (typeAstExpressions scope) _children
 
 
 ------------------------BinaryOperation-------------------------------------------------------
-addTypes scope (Node (AstExpression (Expression _ (BinaryOperation operator e1 e2))) _children)
+typeAstExpressions scope (Node (AstExpression (Expression _ (BinaryOperation operator e1 e2))) _children)
   | and [(expressionType typedE1 ==  expressionType typedE2), (innerType (expressionType typedE1) == Int), not((isArray (expressionType typedE1))), (operator `elem` [Multiply, Divide, Modulus])]= typedExpression
   | and [(expressionType typedE1 ==  expressionType typedE2), (innerType (expressionType typedE1) == Short), not((isArray (expressionType typedE1)))]= typedExpression
   | otherwise = error "Mutilply parameters are not the same and is invalid"
   where
     typedExpression = Node (AstExpression $ Expression _type (BinaryOperation Multiply typedE1 typedE2)) newChildren
-    newChildren = map (addTypes scope) _children
+    newChildren = map (typeAstExpressions scope) _children
     _type = expressionType e1
-    typedE1 = e1 -- addTypes scope e1
-    typedE2= e2 -- addTypes scope e2 
+    typedE1 = e1 -- typeAstExpressions scope e1
+    typedE2= e2 -- typeAstExpressions scope e2
 
-addTypes scope (Node n _children) = (Node n newChildren)
+typeAstExpressions scope (Node n _children) = (Node n newChildren)
   where
-    newChildren = map (addTypes scope) _children
-
-
+    newChildren = map (typeAstExpressions scope) _children
 
 -- TODO
 calculateType :: Expression -> Type
