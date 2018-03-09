3e9f692f69c373c8c4a4ba346031b3d83dc3aab6
Ahmed Al-Sudani <ahmed.al-sudani@uwaterloo.ca>
2018-03-09 15:55:01 -0500

    Copy over type utilityes to Types.hs

--- src/compiler/Types.hs
+++ src/compiler/Types.hs
@@ -14,3 +14,41 @@ import JoosCompiler.TreeUtils
 
 getType :: WholeProgram -> Scope -> Expression -> Type
 getType _ _ _ = Type Int False
+
+isName :: InnerType -> Bool
+isName (NamedType _) = True
+isName _ = False
+
+getLiteralType :: Literal -> Type
+getLiteralType (IntegerLiteral _) = _type
+  where
+    _type = Type
+               {innerType = Int
+               ,isArray = False
+               }
+getLiteralType (BooleanLiteral _) = _type
+  where
+    _type = Type
+               {innerType = Boolean
+               ,isArray = False
+               }
+
+getLiteralType (CharacterLiteral _) = _type
+  where
+    _type = Type
+               {innerType = Char
+               ,isArray = False
+               }
+
+getLiteralType (StringLiteral _) = _type
+  where
+    _type = Type
+               {innerType =  NamedType (["java","lang","String"])
+               ,isArray = False
+               }
+
+getLiteralType (NullLiteral) = _type
+  where
+    _type = Null
+
+getLiteralType _ = error "Unhandled case for getLiteralType"
