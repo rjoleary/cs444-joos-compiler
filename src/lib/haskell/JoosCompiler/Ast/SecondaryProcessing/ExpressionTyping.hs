9799f8304d140c12239805b012d0c86c65974772
Xia Liu <x397liu@uwaterloo.ca>
2018-03-08 23:38:49 -0500

    Add logical operators type checking

--- src/lib/haskell/JoosCompiler/Ast/SecondaryProcessing/ExpressionTyping.hs
+++ src/lib/haskell/JoosCompiler/Ast/SecondaryProcessing/ExpressionTyping.hs
@@ -132,6 +132,31 @@ typeExpression cu scope (Expression _ (BinaryOperation operator e1 e2))
     typedE1 = typeExpression cu scope e1
     typedE2 = typeExpression cu scope e2
 
+typeExpression cu scope (Expression _ (BinaryOperation operator e1 e2))
+  | and [(operator `elem` [And, Or]),
+        not (isArray (expressionType typedE1)),
+        not (isArray (expressionType typedE2)),
+        ((innerType (expressionType typedE1)) == (innerType (expressionType typedE2))),
+        ((innerType (expressionType typedE1)) `elem` [Byte, Int, Short])] =
+      Expression _type1 (BinaryOperation operator typedE1 typedE2)
+  | and [(operator `elem` [And, Or]),
+        not (isArray (expressionType typedE1)),
+        not (isArray (expressionType typedE2)),
+        ((innerType (expressionType typedE1)) == (innerType (expressionType typedE2))),
+        ((innerType (expressionType typedE1)) == Boolean)] =
+      Expression _type2 (BinaryOperation operator typedE1 typedE2)
+  | otherwise = error "Logical operators expressions are invalid"
+  where
+    typedE1 = typeExpression cu scope e1
+    typedE2 = typeExpression cu scope e2
+    _type1 = Type
+            {innerType = Int
+            ,isArray = False
+            }
+    _type2 = Type
+            {innerType = Boolean
+            ,isArray = False
+            }
 
 
 
