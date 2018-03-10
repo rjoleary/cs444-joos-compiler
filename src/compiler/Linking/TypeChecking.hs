ca9e933c05a713b5632926a0d00baebb788af87f
Ahmed Al-Sudani <ahmed.al-sudani@uwaterloo.ca>
2018-03-09 19:43:15 -0500

    Do not type check java packages

--- src/compiler/Linking/TypeChecking.hs
+++ src/compiler/Linking/TypeChecking.hs
@@ -33,6 +33,11 @@ findScopedExpressions :: AstNode -> [(Scope, Expression)]
 findScopedExpressions t = f (Scope [] Nothing []) t
   where
     f :: Scope -> AstNode -> [(Scope, Expression)]
+    f scope (Node (AstCompilationUnit unit) children)
+      | length pName > 0 && head pName == "java" = []
+      | otherwise = mconcat $ map (f scope) children
+      where
+        pName = cuPackage unit
     f scope (Node (AstExpression e) children) = (scope, e) : (mconcat $ map (f scope) children)
     f _ (Node (AstBlock (Block scope)) children) = mconcat $ map (f scope) children
     f scope (Node _ children) = mconcat $ map (f scope) children
