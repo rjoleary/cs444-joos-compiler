402cace5f9d82b2f3699c13bf08b7b3b14f5c4be
Ahmed Al-Sudani <ahmed.al-sudani@uwaterloo.ca>
2018-03-19 09:44:24 -0400

    Delete old Types

--- src/compiler/Linking/TypeChecking.hs
+++ src/compiler/Linking/TypeChecking.hs
@@ -20,7 +20,6 @@ import JoosCompiler.Ast.Visitor.Analysis
 import JoosCompiler.Ast.Utils
 import JoosCompiler.Error
 import JoosCompiler.TreeUtils
-import Types
 
 checkTypes :: AstNode -> Either String ()
 checkTypes (Node x _) = analyze (TypeAnalysis Map.empty) x
