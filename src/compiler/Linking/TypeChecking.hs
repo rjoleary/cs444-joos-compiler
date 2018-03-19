78ec23cc1a97bfe4c630b9341f1988dd1bb66ab5
Ryan O'Leary <ryan.oleary@uwaterloo.ca>
2018-03-19 07:49:51 -0400

    Use visitor pattern in type checking code

--- src/compiler/Linking/TypeChecking.hs
+++ src/compiler/Linking/TypeChecking.hs
@@ -1,78 +1,274 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Linking.TypeChecking
   ( checkTypes
   ) where
 
-import Data.Tree
-import Data.List
-import Data.Function
-import Data.List.Unique
 import Control.Monad
+import Data.Char
+import Data.Either
+import Data.Function
+import Data.List
+import Data.List.Unique
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Tree
 import JoosCompiler.Ast
-import JoosCompiler.Ast.NodeTypes
 import JoosCompiler.Ast.NodeFunctions
+import JoosCompiler.Ast.NodeTypes
 import JoosCompiler.Ast.Transformers.Types
+import JoosCompiler.Ast.Visitor.Analysis
 import JoosCompiler.Ast.Utils
 import JoosCompiler.Error
 import JoosCompiler.TreeUtils
-import Linking.Types
 
--- Returns a error message if hierarchy checking fails.
 checkTypes :: AstNode -> Either String ()
-checkTypes ast@(Node (AstWholeProgram program) children) =
-  (foldEither $ flatten $ fmap checkStatementType $ stdFilter ast) >> return ()
+checkTypes (Node x _) = analyze (TypeAnalysis Map.empty) x
 
-  where
-    stdFilter (Node x@(AstCompilationUnit (CompilationUnit{cuPackage=("java":_)})) _) = Node x []
-    stdFilter (Node x xs) = Node x (map stdFilter xs)
+-- For local types only.
+type LocalEnvironment = Map.Map String Type
 
-    expressions = findScopedExpressions ast
+-- TODO: add global environment
+data TypeAnalysis = TypeAnalysis
+  { localEnvironment :: LocalEnvironment }
 
-    getScope :: Expression -> Scope
-    getScope expr = fst $ head $ filter (\(_, e) -> e == expr) expressions
+-- Analysis on classes/methods/statements. No Type is returned.
+instance Analysis TypeAnalysis () where
+  -- TODO: This skips type checking on standard libraries. However, it should pass.
+  analyze ctx (AstCompilationUnit (CompilationUnit{cuPackage=("java":_)})) =
+    return ()
 
-    -- Short form
-    getExprType' e = getExpressionType program (getScope e) e
+  analyze ctx (AstStatement s@ExpressionStatement{}) = do
+    -- The type annotation is required here because the return is ignored.
+    exprType <- analyze' ctx (statementExpression s) :: Either String Type
+    return ()
 
-    checkStatementType :: AstWrapper -> Either String ()
+  analyze ctx (AstStatement s@LoopStatement{}) = do
+    predicateType <- analyze' ctx (loopPredicate s)
+    when (not $ isBoolean $ predicateType)
+      (Left "Loop predicate must be a boolean")
 
-    -- ExpressionStatement
-    checkStatementType (AstStatement s@ExpressionStatement{}) = do
-      exprType <- getExprType' $ statementExpression s
-      return ()
+  analyze ctx (AstStatement s@IfStatement{}) = do
+    predicateType <- analyze' ctx (ifPredicate s)
+    when (not $ isBoolean $ predicateType)
+      (Left "If predicate must be a boolean")
 
-    -- LoopStatement
-    checkStatementType (AstStatement s@LoopStatement{}) = do
-      predicateType <- getExprType' $ loopPredicate s
-      when (not $ isBoolean $ predicateType)
-        (Left "Loop predicate must be a boolean")
+  analyze ctx (AstStatement ReturnStatement{returnExpression=Just e}) = do
+    returnType <- analyze' ctx e
+    when (returnType == Void) (Left "Cannot return void")
 
-    -- IfStatement
-    checkStatementType (AstStatement s@IfStatement{}) = do
-      predicateType <- getExprType' $ ifPredicate s
-      when (not $ isBoolean $ predicateType)
-        (Left "If predicate must be a boolean")
+  -- TODO: add local variable type to scope
+  analyze ctx (AstStatement LocalStatement{localVariable=l}) = do
+    exprType <- analyze' ctx (variableValue l)
+    when (exprType /= variableType l) (Left "Local statement type doesn't match")
 
-    -- ReturnStatement
-    checkStatementType (AstStatement ReturnStatement{returnExpression=Just e}) = do
-      returnType <- getExprType' $ e
-      when (returnType == Void) (Left "Cannot return void")
+  analyze ctx x = propagateAnalyze ctx x
 
-    -- LocalStatement
-    checkStatementType (AstStatement LocalStatement{localVariable=l}) = do
-      exprType <- getExprType' $ variableValue l
-      when (exprType /= variableType l) (Left "Local statement type doesn't match")
+-- Analysis on expressions. Type is returned.
+instance Analysis TypeAnalysis Type where
+  -- JLS 15.8.1: Lexical Literals
+  analyze ctx (AstExpression (LiteralExpression t))
+    = Right (literalType t)
 
-    checkStatementType _ = return ()
+  -- JLS 15.8.3: this
+  analyze ctx (AstExpression This)
+    = Right (Type (NamedType ["java", "lang", "String"]) False) -- TODO: resolve to this class
 
-findScopedExpressions :: AstNode -> [(Scope, Expression)]
-findScopedExpressions t = f (Scope [] Nothing []) t
-  where
-    f :: Scope -> AstNode -> [(Scope, Expression)]
-    f scope (Node (AstCompilationUnit unit) children)
-      | length pName > 0 && head pName == "java" = []
-      | otherwise = mconcat $ map (f scope) children
-      where
-        pName = cuPackage unit
-    f scope (Node (AstExpression e) children) = (scope, e) : (mconcat $ map (f scope) children)
-    f _ (Node (AstBlock (Block scope)) children) = mconcat $ map (f scope) children
-    f scope (Node _ children) = mconcat $ map (f scope) children
+  -- JLS 15.9: Class Instance Creation Expressions
+  analyze ctx (AstExpression e@(NewExpression name arguments))
+    = if foundConstructor
+      then Right (Type Int False)
+      else Left ("Could not find a matching constructor" ++ show e)
+    where foundConstructor = and $ map (isRight . analyze'') arguments
+          analyze'' x = analyze' ctx x :: Either String Type
+  -- TODO: the previous just checks if the arguments are not error types.
+  -- TODO: we also need to lookup the constructor and overload
+
+  -- JLS 15.10: Array Creation Expressions
+  analyze ctx (AstExpression e@(NewArrayExpression t sizeExpr)) = do
+    sizeType <- analyze' ctx sizeExpr
+    if isNumeric sizeType
+      then return (toArray t)
+      else Left ("Array size must be numeric type " ++ show e)
+
+  -- JLS 15.11: Field Access Expressions
+  analyze ctx (AstExpression e@(FieldAccess primary name)) = do
+    classType <- analyze' ctx primary
+    if not $ isName classType -- TODO: arrays have a length field
+      then Left ("Can only access fields of reference types " ++ show e)
+      else return $ Type Int False
+      --else return $ resolveToType wp s name -- TODO: wrong resolve
+
+  -- JLS 15.12: Method Invocation Expressions
+  analyze ctx (AstExpression e@(MethodInvocation expr name argExprs)) = do
+    exprType <- analyze' ctx expr
+    when (not $ isReference exprType)
+      (Left $ "Method may only be invoked on reference types " ++ show e)
+    argTypes <- foldEither $ map (analyze' ctx) argExprs
+    let lookupSignature = createLookupSignature name argTypes
+    return (Type Int False) -- TODO: lookup
+
+  -- JLS 15.13: Array Access Expressions
+  analyze ctx (AstExpression e@(ArrayExpression arrayExpr sizeExpr)) = do
+    arrayType <- analyze' ctx arrayExpr
+    sizeExpr <- analyze' ctx sizeExpr
+    if not $ isArray arrayType
+      then Left ("Can only perform array access on an array " ++ show e)
+      else do
+        if not $ isNumeric sizeExpr
+          then Left ("Can only perform array access with numbers " ++ show e)
+          else return (toScalar arrayType)
+
+  -- JLS 15.15.4: Unary Minus Operator (-)
+  analyze ctx (AstExpression e@(UnaryOperation Negate expr)) = do
+    exprType <- analyze' ctx expr
+    if not $ isNumeric exprType
+      then Left ("Can only negate numeric types " ++ show e)
+      else return (Type Int False) -- promotion
+
+  -- JLS 15.15.6: Logical Complement Operator (!)
+  analyze ctx (AstExpression e@(UnaryOperation Not expr)) = do
+    exprType <- analyze' ctx expr
+    if not $ isBoolean exprType
+      then Left ("Can only not boolean types" ++ show e)
+      else return exprType
+
+  -- JLS 15.16: Cast Expressions
+  analyze ctx (AstExpression e@(CastExpression t expr)) = do
+    exprType <- analyze' ctx expr :: Either String Type
+    return $ t -- TODO: there are more rules
+
+  -- Binary Operations
+  analyze ctx (AstExpression e@(BinaryOperation op expr1 expr2))
+
+    -- JLS 15.17: Multiplicative Operators (*, /, %)
+    | op `elem` [Multiply, Divide, Modulus] = do
+      expr1Type <- analyze' ctx expr1
+      expr2Type <- analyze' ctx expr2
+      if isNumeric expr1Type && isNumeric expr2Type
+        then return (Type Int False)
+        else Left ("Bad multiplicative types" ++ show e)
+
+    -- JLS 15.18.1: String Concatenation Operator (+)
+    -- JLS 15.18.2: Additive Operators (+) for Numeric Types
+    | op `elem` [Add] = do
+      expr1Type <- analyze' ctx expr1
+      expr2Type <- analyze' ctx expr2
+      if isString expr1Type || isString expr2Type
+        then return (Type (NamedType ["java", "lang", "String"]) False)
+        else if isNumeric expr1Type && isNumeric expr2Type
+          then return (Type Int False) -- TODO: promotions
+          else Left ("Bad addition types " ++ show e)
+
+    -- JLS 15.18.2: Additive Operators (-) for Numeric Types
+    | op `elem` [Subtract] = do
+      expr1Type <- analyze' ctx expr1
+      expr2Type <- analyze' ctx expr2
+      if isNumeric expr1Type && isNumeric expr2Type
+        then return (Type Int False)
+        else Left ("Bad Subtract types " ++ show e)
+
+    -- JLS 15.20: Relational Operators (<, >, <=, >=)
+    | op `elem` [Less, Greater, LessEqual, GreaterEqual] = do
+      expr1Type <- analyze' ctx expr1
+      expr2Type <- analyze' ctx expr2
+      if (isNumeric expr1Type && isNumeric expr2Type)
+        then return (Type Boolean False) -- TODO: more checks are required
+        else Left ("Bad binary operator " ++ show e)
+
+    -- JLS 15.21: Equality Operators (==, !=)
+    | op `elem` [Equality, Inequality] = do
+      expr1Type <- analyze' ctx expr1
+      expr2Type <- analyze' ctx expr2
+      if ((expr1Type :: Type) == (expr2Type :: Type))
+         then return (Type Boolean False)
+         else Left ("Bad Equality types " ++ show e)
+
+    -- JLS 15.22.2: Boolean Logical Operators &, ^, and |
+    -- JLS 15.23: Conditional-And Operator (&&)
+    -- JLS 15.24: Conditional-Or Operator (||)
+    | op `elem` [LazyAnd, LazyOr, And, Or] = do
+      expr1Type <- analyze' ctx expr1
+      expr2Type <- analyze' ctx expr2
+      if (isBoolean expr1Type && isBoolean expr2Type)
+        then return (Type Boolean False) -- TODO: more checks are required
+        else Left ("Bad binary operator " ++ show e)
+
+    -- JLS 15.26: Assignment Operators (=)
+    | op `elem` [Assign] = do
+      expr1Type <- analyze' ctx expr1
+      expr2Type <- analyze' ctx expr2
+      if expr1Type == expr2Type
+        then return expr1Type -- TODO: more checks are required
+        else Left ("Bad binary operator " ++ show e)
+
+  -- JLS 15.20.2: Type Comparison Operator instanceof
+  analyze ctx (AstExpression e@(InstanceOfExpression expr t)) = do
+    exprType <- analyze' ctx expr
+    if (isReference exprType || exprType == Null) && (isReference t)
+      then return (Type Boolean False)
+      else Left ("Bad instanceof operator " ++ show e)
+
+  {-
+
+  analyze ctx (ExpressionName name) =
+    return $ fromMaybe localType typeType
+    where typeDecl = resolveTypeFromProgram wp name
+          typeType = fmap (\x -> Type (NamedType [typeName x]) False) typeDecl
+          localType = resolveToType wp s (showName name)
+
+  -}
+
+  analyze ctx (AstExpression (ExpressionName name)) =
+    return $ Type Int False -- TODO: this is wrong
+
+---------- Helper Functions ----------
+
+toScalar :: Type -> Type
+toScalar (Type x _) = Type x False
+
+toArray :: Type -> Type
+toArray (Type x _) = Type x True
+
+isName :: Type -> Bool
+isName (Type (NamedType _) False) = True
+isName _                          = False
+
+isNumeric :: Type -> Bool
+isNumeric (Type Char False)  = True
+isNumeric (Type Byte False)  = True
+isNumeric (Type Int False)   = True
+isNumeric (Type Short False) = True
+isNumeric _                  = False
+
+isReference :: Type -> Bool
+isReference Null                   = True
+isReference (Type _ True)          = True
+isReference (Type (NamedType _) _) = True
+isReference _                      = False
+
+isPrimitive :: Type -> Bool
+isPrimitive (Type Boolean False) = True
+isPrimitive (Type Byte False)    = True
+isPrimitive (Type Char False)    = True
+isPrimitive (Type Int False)     = True
+isPrimitive (Type Short False)   = True
+isPrimitive _                    = False
+
+isBoolean :: Type -> Bool
+isBoolean (Type Boolean False) = True
+isBoolean _                    = False
+
+isString :: Type -> Bool
+isString t = typeSignature t == "java.lang.String"
+
+-- Create method signature suitable for lookup.
+createLookupSignature :: String -> [Type] -> String
+createLookupSignature name args =
+  methodSignature $ Method Void [] name (map (\t -> Variable t [] "" $ This) args) TerminalStatement
+
+leftOrRight :: Either a a -> a
+leftOrRight (Left x) = x
+leftOrRight (Right x) = x
+
+resolveToType :: WholeProgram -> Scope -> String -> Type
+resolveToType wp s name = variableType $ leftOrRight $ resolveInScope wp s [name]
