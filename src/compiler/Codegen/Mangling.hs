ba79ae21ba625710b9935c3ffc4b8a988f410286
Xia Liu <x397liu@uwaterloo.ca>
2018-03-22 00:41:23 -0400

    Add method part in Mangling.hs

--- src/compiler/Codegen/Mangling.hs
+++ src/compiler/Codegen/Mangling.hs
@@ -24,9 +24,17 @@ mangleField field = mangledCanonical
 
 -- Method$p$q$A$ma#int#java.lang.String#
 mangleMethod :: Method -> String
-mangleMethod method = methodName method
+mangleMethod method = mangledCanonical -- methodName method
   where
     canonicalized = methodCanonicalName method
+    firstPart = intercalate "$" ("Method" : canonicalized)
+    v = methodParameters method
+    t = map (show . variableType) v
+    secondPart = intercalate "#" t
+    prepare = [firstPart, secondPart]
+    mangledCanonical = (intercalate "#" prepare) ++ "#"
+
+
 
 -- Class$p$q$A
 -- Can also be interface
