4cc6046d80fa8c172ac90ae310f01ef0ee298079
Ahmed Al-Sudani <ahmed.al-sudani@uwaterloo.ca>
2018-03-24 16:38:04 -0400

    Add java-experiment: object equality

---
+++ test/java-exploration/object-equality/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+        Integer o1 = 1;
+        Integer o2 = 1;
+        System.out.println("==");
+        System.out.println(o1 == o2);
+        System.out.println("equal");
+        System.out.println(o1.equals(o2));
+    }
+}
