ab79a0b14798f6a926e6cfc4d824a86b5333d172
Ahmed Al-Sudani <ahmed.al-sudani@uwaterloo.ca>
2018-03-24 01:18:55 -0400

    Unique package names
    There was a problem with Unique.unique

--- src/lib/JoosCompiler/Ast/SecondaryProcessing/Packaging.hs
+++ src/lib/JoosCompiler/Ast/SecondaryProcessing/Packaging.hs
@@ -5,11 +5,13 @@ module JoosCompiler.Ast.SecondaryProcessing.Packaging
 
 import           Data.Maybe
 import           Data.List
-import           Data.List.Unique
 import           Data.Tree
 import           JoosCompiler.Ast.NodeTypes
 import           JoosCompiler.Ast.Transformers.Types
 import           JoosCompiler.Ast.Utils
+
+unique :: Eq a => [a] -> [a]
+unique = foldr (\x seen -> if (x `elem` seen) then seen else x:seen) []
 
 packageProgram  :: [AstNode] -> AstNode
 packageProgram unitNodes = Node program unitNodes
@@ -18,16 +20,13 @@ packageProgram unitNodes = Node program unitNodes
     units = map (astCompilationUnit . rootLabel) unitNodes
     program = AstWholeProgram $ WholeProgram groupedByPackages units
 
-startsWithSameName :: (Name, Package) -> (Name, Package) -> Bool
-startsWithSameName p1 p2 = (head $ fst p1) == (head $ fst p2)
-
 -- Assumes that root nodes are CompilationUnits
 groupByPackage :: [AstNode] -> [Package]
 groupByPackage unitNodes = packages
   where
     compilationUnits = map (astCompilationUnit . rootLabel) unitNodes
     -- TODO unique
-    packageNames = map cuPackage compilationUnits
+    packageNames = unique $ map cuPackage compilationUnits
     packages = map groupUnitsByPackageName packageNames
     groupUnitsByPackageName :: Name -> Package
     groupUnitsByPackageName _packageName =
