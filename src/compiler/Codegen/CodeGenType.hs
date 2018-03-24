d9f9afbabcb68aa129a21400aa54a50c44d5ba57
Ryan O'Leary <rj2olear@uwaterloo.ca>
2018-03-24 15:10:55 -0400

    Add generate expression

--- src/compiler/Codegen/CodeGenType.hs
+++ src/compiler/Codegen/CodeGenType.hs
@@ -30,3 +30,52 @@ instance Analysis CodeGenType (Asm ()) where
 
   -- Everything else propagates.
   analyze ctx x = propagateAnalyze ctx x
+
+data Context
+
+generateExpression :: Context -> Expression -> Asm Type
+generateExpression ctx (BinaryOperation Multiply x y) = do
+  t1 <- generateExpression ctx x
+  push Eax
+  t2 <- generateExpression ctx y
+  mov Ebx Eax
+  pop Eax
+  imul Eax Ebx
+  return (Type Int False) -- TODO: strings
+
+generateExpression ctx (BinaryOperation Modulus x y) = do
+  t1 <- generateExpression ctx x
+  push Eax
+  t2 <- generateExpression ctx y
+  mov Ebx Eax
+  pop Eax
+  idiv Ebx
+  mov Edx Eax
+  return (Type Int False)
+
+generateExpression ctx (BinaryOperation Divide x y) = do
+  t1 <- generateExpression ctx x
+  push Eax
+  t2 <- generateExpression ctx y
+  mov Ebx Eax
+  pop Eax
+  idiv Ebx
+  return (Type Int False)
+
+generateExpression ctx (BinaryOperation Add x y) = do
+  t1 <- generateExpression ctx x
+  push Eax
+  t2 <- generateExpression ctx y
+  mov Ebx Eax
+  pop Eax
+  add Eax Ebx
+  return (Type Int False)
+
+generateExpression ctx (BinaryOperation Subtract x y) = do
+  t1 <- generateExpression ctx x
+  push Eax
+  t2 <- generateExpression ctx y
+  mov Ebx Eax
+  pop Eax
+  sub Eax Ebx
+  return (Type Int False)
