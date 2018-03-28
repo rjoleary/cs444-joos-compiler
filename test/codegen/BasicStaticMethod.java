e237e47725f75d5ec7b35081bb7e2230ac9bf286
Ryan O'Leary <rj2olear@uwaterloo.ca>
2018-03-28 18:59:38 -0400

    Add basic method test

---
+++ test/codegen/BasicStaticMethod.java
@@ -0,0 +1,11 @@
+public class BasicStaticMethod {
+    public static BasicStaticMethod() {}
+
+    public static int f() {
+        return 123;
+    }
+
+    public static int test() {
+        return f();
+    }
+}
